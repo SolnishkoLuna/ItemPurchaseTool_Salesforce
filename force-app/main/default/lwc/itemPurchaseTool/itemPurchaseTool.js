be2b21deee430e5da6690e291502c7c9302111be
Natali <ichkitidzenatali@gmal.com>
2026-02-21 19:21:06 +0400

    Implemented functional product search. Connected LWC to Apex with dynamic results rendering.

--- force-app/main/default/lwc/itemPurchaseTool/itemPurchaseTool.js
+++ force-app/main/default/lwc/itemPurchaseTool/itemPurchaseTool.js
@@ -1,5 +1,37 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import getProducts from '@salesforce/apex/ItemPurchaseController.getProducts';
 
 export default class ItemPurchaseTool extends LightningElement {
     @api recordId;
+    @track products = [];
+    @track error;
+
+    searchTerm = '';
+    selectedFamily = '';
+    selectedType = '';
+
+    handleSearchChange(event) {
+        this.searchTerm = event.target.value;
+        this.loadProducts();
+    }
+
+    loadProducts() {
+        getProducts({
+            searchTerm: this.searchTerm,
+            family: this.selectedFamily,
+            type: this.selectedType
+        })
+        .then(result => {
+            this.products = result;
+            this.error = undefined;
+        })
+        .catch(error => {
+            this.error = error;
+            this.products = undefined;
+        });
+    }
+
+    connectedCallback() {
+        this.loadProducts();
+    }
 }
